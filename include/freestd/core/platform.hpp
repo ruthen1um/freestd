4fc5c10143565f4a8d30d6d023c82687fb53be20
Oleg Fedotov <ol.fed25@gmail.com>
2026-08-15 12:12:31 +0300

    update platform detection code

--- include/freestd/core/platform.hpp
+++ include/freestd/core/platform.hpp
@@ -2,15 +2,19 @@
 #define FREESTD_CORE_PLATFORM_HPP
 
 #if defined(__x86_64__)
-    #define FREESTD_ARCH_X86_64 1
-#else
-    #error "Unsupported architecture for freestd (only x86-64 is supported)"
+#define FREESTD_ARCH_X86_64 1
 #endif
 
 #if defined(__linux__)
-    #define FREESTD_OS_LINUX 1
-#else
-    #error "Unsupported OS for freestd (only Linux is supported)"
+#define FREESTD_OS_LINUX 1
+#endif
+
+#if defined(FREESTD_OS_LINUX)
+#define FREESTD_CURRENT_OS_NAME "linux"
+#endif
+
+#if defined(FREESTD_ARCH_X86_64)
+#define FREESTD_CURRENT_ARCH_NAME "x86-64"
 #endif
 
 #endif // FREESTD_CORE_PLATFORM_HPP
