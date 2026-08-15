35ae9556606282192bf44347f0ad9bec472afaef
Oleg Fedotov <ol.fed25@gmail.com>
2026-08-15 12:11:03 +0300

    improve result implementation

--- include/freestd/core/result.hpp
+++ include/freestd/core/result.hpp
@@ -6,31 +6,31 @@ namespace freestd::core {
     class Result {
     public:
         constexpr explicit Result(T ok) noexcept
-            : m_value(ok), m_state(State::Ok) {}
+            : value{ok}, state{State::Ok} {}
 
         constexpr explicit Result(E err) noexcept
-            : m_value(err), m_state(State::Err) {}
+            : value{err}, state{State::Err} {}
 
+        // TODO: figure out what to do with those constructors and assignment operators
         constexpr Result(const Result<T, E>& other) noexcept = delete;
         constexpr Result& operator=(const Result<T, E>& other) noexcept = delete;
-
         constexpr Result(Result<T, E>&& other) noexcept = delete;
         constexpr Result& operator=(Result<T, E>&& other) noexcept = delete;
 
-        [[nodiscard]] constexpr T unwrap() const noexcept {
-            return m_value.ok;
+        [[nodiscard]] constexpr T unwrap(this auto&& self) noexcept {
+            return self.value.ok;
         }
 
-        [[nodiscard]] constexpr E unwrap_err() const noexcept {
-            return m_value.err;
+        [[nodiscard]] constexpr E unwrap_err(this auto&& self) noexcept {
+            return self.value.err;
         }
 
-        [[nodiscard]] constexpr bool is_ok() const noexcept {
-            return m_state == State::Ok;
+        [[nodiscard]] constexpr bool is_ok(this auto&& self) noexcept {
+            return self.state == State::Ok;
         }
 
-        [[nodiscard]] constexpr bool is_err() const noexcept {
-            return m_state == State::Err;
+        [[nodiscard]] constexpr bool is_err(this auto&& self) noexcept {
+            return self.state == State::Err;
         }
 
     private:
@@ -44,186 +44,14 @@ namespace freestd::core {
             E err;
 
             constexpr Value(T value) noexcept
-                : ok(value) {}
+                : ok{value} {}
 
             constexpr Value(E value) noexcept
-                : err(value) {}
+                : err{value} {}
         };
 
-        State m_state;
-        Value m_value;
-    };
-
-    template <typename T, typename E>
-    class Result<T&, E> {
-    public:
-        constexpr explicit Result(T& ok) noexcept
-            : m_value(ok), m_state(State::Ok) {}
-
-        constexpr explicit Result(E err) noexcept
-            : m_value(err), m_state(State::Err) {}
-
-        constexpr Result(const Result<T&, E>& other) noexcept = delete;
-        constexpr Result& operator=(const Result<T&, E>& other) noexcept = delete;
-
-        constexpr Result(Result<T&, E>&& other) noexcept = delete;
-        constexpr Result& operator=(Result<T&, E>&& other) noexcept = delete;
-
-        [[nodiscard]] constexpr const T& unwrap() const noexcept {
-            return *m_value.ok;
-        }
-
-        [[nodiscard]] constexpr T& unwrap() noexcept {
-            return *m_value.ok;
-        }
-
-        [[nodiscard]] constexpr E unwrap_err() const noexcept {
-            return m_value.err;
-        }
-
-        [[nodiscard]] constexpr bool is_ok() const noexcept {
-            return m_state == State::Ok;
-        }
-
-        [[nodiscard]] constexpr bool is_err() const noexcept {
-            return m_state == State::Err;
-        }
-
-    private:
-        enum class State {
-            Ok,
-            Err,
-        };
-
-        union Value {
-            T* ok;
-            E err;
-
-            constexpr Value(T& value) noexcept
-                : ok(&value) {}
-
-            constexpr Value(E value) noexcept
-                : err(value) {}
-        };
-
-        State m_state;
-        Value m_value;
-    };
-
-    template <typename T, typename E>
-    class Result<T, E&> {
-    public:
-        constexpr explicit Result(T ok) noexcept
-            : m_value(ok), m_state(State::Ok) {}
-
-        constexpr explicit Result(E& err) noexcept
-            : m_value(err), m_state(State::Err) {}
-
-        constexpr Result(const Result<T, E&>& other) noexcept = delete;
-        constexpr Result& operator=(const Result<T, E&>& other) noexcept = delete;
-
-        constexpr Result(Result<T, E&>&& other) noexcept = delete;
-        constexpr Result& operator=(Result<T, E&>&& other) noexcept = delete;
-
-        [[nodiscard]] constexpr T unwrap() const noexcept {
-            return m_value.ok;
-        }
-
-        [[nodiscard]] constexpr const E& unwrap_err() const noexcept {
-            return *m_value.err;
-        }
-
-        [[nodiscard]] constexpr E& unwrap_err() const noexcept {
-            return *m_value.err;
-        }
-
-        [[nodiscard]] constexpr bool is_ok() const noexcept {
-            return m_state == State::Ok;
-        }
-
-        [[nodiscard]] constexpr bool is_err() const noexcept {
-            return m_state == State::Err;
-        }
-
-    private:
-        enum class State {
-            Ok,
-            Err,
-        };
-
-        union Value {
-            T ok;
-            E* err;
-
-            constexpr Value(T value) noexcept
-                : ok(value) {}
-
-            constexpr Value(E& value) noexcept
-                : err(&value) {}
-        };
-
-        State m_state;
-        Value m_value;
-    };
-
-    template <typename T, typename E>
-    class Result<T&, E&> {
-    public:
-        constexpr explicit Result(T& ok) noexcept
-            : m_value(ok), m_state(State::Ok) {}
-
-        constexpr explicit Result(E& err) noexcept
-            : m_value(err), m_state(State::Err) {}
-
-        constexpr Result(const Result<T&, E&>& other) noexcept = delete;
-        constexpr Result& operator=(const Result<T&, E&>& other) noexcept = delete;
-
-        constexpr Result(Result<T&, E&>&& other) noexcept = delete;
-        constexpr Result& operator=(Result<T&, E&>&& other) noexcept = delete;
-
-        [[nodiscard]] constexpr const T& unwrap() const noexcept {
-            return *m_value.ok;
-        }
-
-        [[nodiscard]] constexpr T& unwrap() noexcept {
-            return *m_value.ok;
-        }
-
-        [[nodiscard]] constexpr const E& unwrap_err() const noexcept {
-            return *m_value.err;
-        }
-
-        [[nodiscard]] constexpr E& unwrap_err() const noexcept {
-            return *m_value.err;
-        }
-
-        [[nodiscard]] constexpr bool is_ok() const noexcept {
-            return m_state == State::Ok;
-        }
-
-        [[nodiscard]] constexpr bool is_err() const noexcept {
-            return m_state == State::Err;
-        }
-
-    private:
-        enum class State {
-            Ok,
-            Err,
-        };
-
-        union Value {
-            T* ok;
-            E* err;
-
-            constexpr Value(T& value) noexcept
-                : ok(&value) {}
-
-            constexpr Value(E& value) noexcept
-                : err(&value) {}
-        };
-
-        State m_state;
-        Value m_value;
+        State state;
+        Value value;
     };
 } // namespace freestd::core
 
